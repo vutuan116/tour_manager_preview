b7d451edb3503a6c3b4862983fec99eec095754c
vutuan116 <45684720+vutuan116@users.noreply.github.com>
2024-06-09 23:35:50 +0700

    update

--- js/common.js
+++ js/common.js
@@ -2,3 +2,7 @@ function zoomSearchBody() {
     $('#search_body').toggleClass('d-none');
     $('.btn_search_zoom').toggleClass('zoom_out')
 }
+
+function viewDatepicker(id_datepicker) {
+    $("#" + id_datepicker).datepicker();
+}
