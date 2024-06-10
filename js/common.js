86e1f227933592831934c6915d6bbdbb531336ed
vutuan116 <45684720+vutuan116@users.noreply.github.com>
2024-06-11 01:21:24 +0700

    update

--- js/common.js
+++ js/common.js
@@ -1,8 +1,17 @@
+window.onload = function () {
+    if ($(".datepicker").length>0){
+        viewDatepicker();
+    }
+}
+
 function zoomSearchBody() {
     $('#search_body').toggleClass('d-none');
     $('.btn_search_zoom').toggleClass('zoom_out')
 }
 
-function viewDatepicker(id_datepicker) {
-    $("#" + id_datepicker).datepicker();
+//moneyFormatter.format(100000) => 100.000
+const moneyFormatter = new Intl.NumberFormat('vi-VN');
+
+function viewDatepicker() {
+    $(".datepicker").datepicker({dateFormat: "dd/mm/yy"});
 }
